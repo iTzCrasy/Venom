d76e8b2941b6aea1182ce63e9b399f3a7304622c
Adrian Eble <a.eble.p2@online.de>
2019-03-17 02:33:08 +0100

    - Added Loading

--- Venom/Global.cs
+++ Venom/Global.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using Castle.Windsor;
+using Venom.Game.Resources;
 
 namespace Venom
 {
@@ -20,13 +22,17 @@ namespace Venom
         public static void Initialize()
         {
             //=> Setup Windows
-            _Container.Register( Castle.MicroKernel.Registration.Component.For<Windows.StartWindow>( ) );
-            _Container.Register( Castle.MicroKernel.Registration.Component.For<Windows.MainWindow>( ) );
+            _Container.Register( Castle.MicroKernel.Registration.Component.For<Windows.StartWindow>( ).LifestyleSingleton( ) );
+            _Container.Register( Castle.MicroKernel.Registration.Component.For<Windows.MainWindow>( ).LifestyleSingleton( ) );
 
             //=> Setup Views
-            _Container.Register( Castle.MicroKernel.Registration.Component.For<Views.TroupList>( ) );
-            _Container.Register( Castle.MicroKernel.Registration.Component.For<Views.RankingPlayerView>( ) );
-            _Container.Register( Castle.MicroKernel.Registration.Component.For<Views.RankingAllyView>() );
+            _Container.Register( Castle.MicroKernel.Registration.Component.For<Views.TroupList>( ).LifestyleSingleton( ) );
+            _Container.Register( Castle.MicroKernel.Registration.Component.For<Views.RankingPlayerView>( ).LifestyleSingleton( ) );
+            _Container.Register( Castle.MicroKernel.Registration.Component.For<Views.RankingAllyView>().LifestyleSingleton( ) );
+
+            //=> Setup Resources
+            _Container.Register( Castle.MicroKernel.Registration.Component.For<PlayerResource>( ).LifestyleSingleton( ) );
+            _Container.Register( Castle.MicroKernel.Registration.Component.For<AllyResource>( ).LifestyleSingleton( ) );
         }
 
         public static void Shutdown()
@@ -40,25 +46,52 @@ namespace Venom
             WindowStart.Show( );    //=> Show first window
         }
 
-        public static void StartVenom()
+        public static async void StartVenom()
         {
+            var Watch = new Stopwatch( );
+            Watch.Start( );
+
             //=> TODO: Implement loading here!
+            var resources = new IResource[]
+            {
+                PlayerResource,
+                AllyResource
+            };
+
+            var taskList = new List<Task>( );
+            foreach( var i in resources )
+            {
+                taskList.Add( i.Initialize( Core.Game.GetInstance.GetSelectedServer() ) );
+            }
+
+            await Task.WhenAll( taskList );
+
+            Watch.Stop( );
+            Debug.WriteLine( "New Time: " + Watch.ElapsedMilliseconds );
 
             WindowStart.Close( );   //=> Loading finished, close main window
             WindowMain.Show( );     //=> Show main window
         }
 
+        //=> Windows
         public static Windows.StartWindow WindowStart =>
             _Container.Resolve<Windows.StartWindow>( );
 
         public static Windows.MainWindow WindowMain =>
             _Container.Resolve<Windows.MainWindow>( );
 
+        //=> Views
         public static Views.TroupList ViewTroupList =>
             _Container.Resolve<Views.TroupList>( );
         public static Views.RankingPlayerView ViewRankingPlayer =>
             _Container.Resolve<Views.RankingPlayerView>( );
         public static Views.RankingAllyView ViewRankingAlly =>
             _Container.Resolve<Views.RankingAllyView>( );
+
+        //=> Resources
+        public static PlayerResource PlayerResource =>
+            _Container.Resolve<PlayerResource>( );
+        public static AllyResource AllyResource =>
+            _Container.Resolve<AllyResource>( );
     }
 }
